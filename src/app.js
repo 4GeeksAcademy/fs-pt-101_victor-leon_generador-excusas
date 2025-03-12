b7750d46f218828e70512833e95e65d32992c806
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-03-12 20:08:55 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import "bootstrap";
-import "./style.css";
+const arrays = arr => arr[Math.floor(Math.random() * arr.length)],
+quien = ['Mi gato', 'Tu madre', 'Hacienda', 'CR7'],
+accion = ['se comió', 'se cagó', 'rompió', 'se fumó'],
+que = ['mi nómina', 'mi Jaiphone', 'un Lamborghini Huracan (que es de pobres)'],
+cuando = ['a final de mes', 'cuando estaba durmiendo', 'mientras hacia burpees', 'mientras cenaba', 'mientras veia la Isla de las Tentaciones'],
+      
+generarExcusa = () => `${arrays(quien)} ${arrays(accion)} ${arrays(que)} ${arrays(cuando)}.`;
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("boton").addEventListener("click", () => {
+  const excusa = generarExcusa();
+  document.getElementById("excusa").textContent = excusa;
+});
